674a2c0941659f522929c4297ea7f0f747a8212d
imhadden <imhadden@gmail.com>
2016-11-29 12:57:40 -0800

    deserializing the board from the view now works

--- hw5/model/CrushClient.cpp
+++ hw5/model/CrushClient.cpp
@@ -6,10 +6,12 @@
 
 #include "../shared/ClientSocket.h"
 #include "../shared/ServerSocket.h"
+#include "CrushMain.h"
+#define BUFSIZE 1024
 
 using namespace std;
 
-int BUFSIZE = 1024;
+//int BUFSIZE = 1024;
 
 int main(int argc, char** argv){
   if (argc != 3){
@@ -27,19 +29,40 @@ int main(int argc, char** argv){
     hw5_net::ClientSocket clientSocket(serverName, serverPort);
     string helloMessage = "{\"action\": \"hello\"}";
     clientSocket.WrappedWrite(helloMessage.c_str(), helloMessage.length());
-    int readCount;
-    cout << "\tRead:" << endl;
+    int curlyBraceCount = 0;
+    char* stringJson = (char*) malloc(sizeof(char) * BUFSIZE);
+    int stringJsonAllocatedSize = sizeof(char) * BUFSIZE;
+    int size = 0; //how much is actually being used
     do {
-      printf("STARTING A READ\n");
       char buf[BUFSIZE];
-      readCount = clientSocket.WrappedRead(buf, BUFSIZE - 1);
-      buf[readCount] = '\0';
-      cout << buf;
-      cout.flush();
-      printf("%d\n", readCount);
-    } while (readCount != 0);
+      int readCount = clientSocket.WrappedRead(buf, BUFSIZE - 1);
+
+      //if allocated space isn't big enough, reallocate
+      if (size + readCount > stringJsonAllocatedSize){
+	stringJson = (char*) realloc(stringJson, stringJsonAllocatedSize + BUFSIZE * sizeof(char));
+	stringJsonAllocatedSize += BUFSIZE * sizeof(char);
+      }
+      //move chars into allocated space and count curly braces to see if we're done or not
+      for (int i = 0; i < readCount; i++){
+	stringJson[size] = buf[i];
+	size++;
+	if (buf[i] == '{'){
+	  curlyBraceCount++;
+	} else if (buf[i] == '}'){
+	  curlyBraceCount--;
+	}
+      }
+    } while (curlyBraceCount != 0);
+    stringJson[size] = '\0';
     cout.flush();
     cout << "done" << endl;
+    printf("stringJson: %s\n", stringJson);
+    CrushMain* gameInstance = deserializeServerMessage(stringJson);
+    if (gameInstance != NULL){
+      printf("success!\n");
+    } else {
+      printf("error!\n");
+    }
   } catch(string errString) {
     cout << errString << endl;
     return 1;
--- hw5/model/CrushMain.h
+++ hw5/model/CrushMain.h
@@ -1,6 +1,184 @@
 #ifndef _CrushMain_h
 #define _CrushMain_h
 
+//The class used to store data about a game
+class CrushMain {
+  public:
+  int neg = -1; // used to represent a template that needs to be fired
+  int gameid; //unique identifier
+  Array2D extensionColor; //the extension grid
+  Array2D boardInitialState; //how many times each square must fire
+  int movesAllowed; //max moves allowed
+  int colors; //max # of distinct colors in extension board
+
+  Array2D boardCandies; //current candies on the board
+  Array2D boardCandyTypes; //the type of each candy
+  Array2D boardState; //how many times each square must fire
+  Array2D boardType; //the type of each candy
+  int movesMade; //how many moves have been made
+  int currentScore; //total amount that state has decreased
+  int* extensionOffset;
+
+CrushMain(int gameid, Array2D extensionColor,
+		       Array2D boardInitialState, int movesAllowed,
+	               int colors, Array2D boardCandies, Array2D boardCandyTypes,
+	               Array2D boardState, int movesMade, int currentScore, 
+		       int extensionOffset[]){
+
+  this->gameid = gameid;
+  this->extensionColor = extensionColor;
+  this->boardInitialState = boardInitialState;
+  this->movesAllowed = movesAllowed;
+  this->colors = colors;
+  this->boardCandies = boardCandies;
+  this->boardCandyTypes = boardCandyTypes;
+  this->boardState = boardState;
+  this->movesMade = movesMade;
+  this->currentScore = currentScore;
+  this->extensionOffset = extensionOffset;
+}
+
+~CrushMain() {
+  freeArray2D(extensionColor, NULL);
+  freeArray2D(boardInitialState, NULL);
+  freeArray2D(boardCandies, NULL);
+  freeArray2D(boardCandyTypes, NULL);
+  freeArray2D(boardState, NULL);
+  free(extensionOffset);
+  //freeArray2D(boardType, NULL);
+}
+
+// Repeats the process of finding and firing templates until no templates are found. Updates moves remaining, etc. accordingly. Returns the number of templates found.
+int updateWithMove(int x1, int y1, int x2, int y2) {
+  if (movesMade >= movesAllowed) {
+    // Too many moves made, can't do anything.
+    return 0;
+  }
+
+  swapArray2D(this->boardCandies, x1, y1, x2, y2);
+
+  int found = 0;
+  int arbitraryMagicNumber = 1000; // max template firings per move
+  while (found < arbitraryMagicNumber && this->findTemplates()) {
+    found += 1;
+    this->fireTemplates();
+  }
+  if (found) {
+    // Move made successfully, increase count.
+    movesMade++;
+  } else {
+    // No templates made so move is invalid.
+    swapArray2D(this->boardCandies, x1, y1, x2, y2);
+  }
+
+  return found;
+}
+
+// Removes all tiles on the board marked as neg, and applies gravity using the extension board. Also updates the score.
+void fireTemplates() {
+  for (int i = this->boardCandies->rows; i >= 0; i--) {
+    for (int j = 0; j <= this->boardCandies->columns; j++) {
+      if (*((int *) getArray2D(this->boardCandies, j, i)) == neg) {
+        int k = i;
+        for (; k < this->boardCandies->rows; k++) {
+          swapArray2D(this->boardCandies, j, k, j, k + 1);
+        }
+
+        setArray2D(this->boardCandies, getArray2D(extensionColor, j, extensionOffset[j] % (extensionColor->rows + 1)), j, k);
+
+        // Update score.
+        int* remaining = (int*) getArray2D(this->boardState, j, i);
+        if (*remaining != 0) {
+          *remaining = *remaining - 1;
+          currentScore += 1;
+        }
+
+        extensionOffset[j] += 1;
+      }
+    }
+  }
+}
+
+// Finds all existing templates on the board. Returns 1 if it finds at least one template, 0 if none are found. Sets all tiles on the board included in a template to neg, indicating that they are ready to fire using fireTemplates.
+int findTemplates() {
+  int result = 0;
+
+  // Vertical 4-in-a-row
+  for (int i = 0; i <= this->boardCandies->rows - 3; i++) {
+    for (int j = 0; j <= this->boardCandies->columns; j++) {
+      int curr = *((int*)getArray2D(this->boardCandies, j, i));
+      if (curr != neg && curr == *((int*)getArray2D(this->boardCandies, j, i + 1)) && curr == *((int*)getArray2D(this->boardCandies, j, i + 2)) && curr == *((int*)getArray2D(this->boardCandies, j, i + 3))) {
+        result = 1;
+        setArray2D(this->boardCandies, &neg, j, i);
+        setArray2D(this->boardCandies, &neg, j, i + 1);
+        setArray2D(this->boardCandies, &neg, j, i + 2);
+        setArray2D(this->boardCandies, &neg, j, i + 3);
+      }
+    }
+  }
+
+  // Horizontal 4-in-a-row
+  for (int i = 0; i <= this->boardCandies->rows; i++) {
+    for (int j = 0; j <= this->boardCandies->columns - 3; j++) {
+      int curr = *((int*)getArray2D(this->boardCandies, j, i));
+      if (curr != neg && curr == *((int*)getArray2D(this->boardCandies, j + 1, i)) && curr == *((int*)getArray2D(this->boardCandies, j + 2, i)) && curr == *((int*)getArray2D(this->boardCandies, j + 3, i))) {
+        result = 1;
+        setArray2D(this->boardCandies, &neg, j, i);
+        setArray2D(this->boardCandies, &neg, j + 1, i);
+        setArray2D(this->boardCandies, &neg, j + 2, i);
+        setArray2D(this->boardCandies, &neg, j + 3, i);
+      }
+    }
+  }
+
+  // Vertical 3-in-a-row
+  for (int i = 0; i <= this->boardCandies->rows - 2; i++) {
+    for (int j = 0; j <= this->boardCandies->columns; j++) {
+      int curr = *((int*)getArray2D(this->boardCandies, j, i));
+      if (curr != neg && curr == *((int*)getArray2D(this->boardCandies, j, i + 1))&& curr == *((int*)getArray2D(this->boardCandies, j, i + 2))) {
+        result = 1;
+        setArray2D(this->boardCandies, &neg, j, i);
+        setArray2D(this->boardCandies, &neg, j, i + 1);
+        setArray2D(this->boardCandies, &neg, j, i + 2);
+      }
+    }
+  }
+ 
+  // Horizontal 3-in-a-row
+  for (int i = 0; i <= this->boardCandies->rows; i++) {
+    for (int j = 0; j <= this->boardCandies->columns - 2; j++) {
+      int curr = *((int*)getArray2D(this->boardCandies, j, i));
+      if (curr != neg && curr == *((int*)getArray2D(this->boardCandies, j + 1, i)) && curr == *((int*)getArray2D(this->boardCandies, j + 2, i))) {
+        result = 1;
+        setArray2D(this->boardCandies, &neg, j, i);
+        setArray2D(this->boardCandies, &neg, j + 1, i);
+        setArray2D(this->boardCandies, &neg, j + 2, i);
+      }
+    }
+  }
+
+  return result;
+}
+};
+
 int playWithSerializedBoard(char* argv);
+
+Array2D deserializeInt2DArrayFromJsonObject(json_t* json);
+
+int deserializeBoardCandiesFromJsonObject(json_t* json, Array2D* colors, Array2D* types);
+
+CrushMain* deserializeHelper(json_t* json);
+
+CrushMain* deserializeServerMessage(char* location);
+
+CrushMain* deserializeGameInstance(char* location);
+
+json_t* serializeArray2DToJsonObject(Array2D array);
+
+json_t* serializeBoardCandiesToJsonObject(Array2D array, Array2D array2);
+
+void serializeGameInstance(char* location);
+
+void instanceCaller(int x1, int y1, int x2, int y2);
 
 #endif /* _CrushMain_h */
